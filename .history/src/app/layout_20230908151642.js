591d6cf8d21c5f111fea533e83433aac0ad11d51
farrelmuhammad <farrelf4@gmail.com>
2023-09-08 15:36:39 +0700

    Add favicon

--- .history/src/app/layout_20230908151642.js
+++ .history/src/app/layout_20230908151642.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,9 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" /> {/* Pastikan path sesuai dengan lokasi favicon Anda */}
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
